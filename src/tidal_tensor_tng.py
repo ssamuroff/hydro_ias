0661ef3fedea5ecf87191f681e738b275eaab27b
Simon Samuroff <ssamurof@andrew.cmu.edu>
2019-12-06 21:56:12 +0000

    local edits

--- src/tidal_tensor_tng.py
+++ src/tidal_tensor_tng.py
@@ -22,8 +22,18 @@ base_dir = '/physics/nkhandai/mb2/snapdir/'
 dens_dir = '/home/rmandelb.proj/ssamurof/mb2_tidal/'
 
 
+def get_trace_free_matrix(I):
+    ndim = len(I[0])
 
-def build_shape_cube(snap, resolution=512, npart=1024):
+    T = np.matrix.trace(I)
+    K = 1./float(ndim) * T * np.identity(ndim)
+
+    I_TF = I - K
+
+    return I_TF
+
+
+def build_shape_cube(snap, resolution=512, npart=1024, average_type='tensor'):
     """
     Computes a density cube for the specified snapshot
     """
@@ -33,14 +43,21 @@ def build_shape_cube(snap, resolution=512, npart=1024):
     boxsize = 205 # TNG300 value, again
     #readheader(base_name, 'boxsize')/1000 # h^-1 Mpc
 
+    print('AVERAGING MODE: %s'%average_type)
+
 
     cat_name = 'data/cats/fits/TNG300-1_%d_non-reduced_galaxy_shapes.fits'%snap
     shape_cat = fi.FITS(cat_name)[1].read()
     ngal = len(shape_cat)
 
     sij = np.zeros((resolution,resolution,resolution,3,3))
-    #sigma = np.zeros((resolution,resolution,resolution,3,3))
+    dsij = np.zeros((resolution,resolution,resolution,3,3))
     num = np.zeros((resolution,resolution,resolution,3,3))
+    A = np.zeros((resolution,resolution,resolution,3,3))
+
+    # global averages
+    dI_global = np.zeros((3,3))
+    I_global = np.zeros((3,3))
 
     X,Y,Z = np.meshgrid(np.arange(0,resolution,1),np.arange(0,resolution,1),np.arange(0,resolution,1))
     B = np.linspace(0,boxsize,resolution+1)
@@ -65,17 +82,81 @@ def build_shape_cube(snap, resolution=512, npart=1024):
         v0=np.linalg.inv(v)
         I = np.dot(v0,np.dot(V,v))
 
+        I_TF = get_trace_free_matrix(I)
+
+
         #print(I)
        # import pdb ; pdb.set_trace()
 
         # add it to the correct cell
-        sij[ix,iy,iz,:,:] += I
+        if (average_type=='tensor'):
+            sij[ix,iy,iz,:,:] += I_TF
+            I_global += I_TF
+        elif (average_type=='tensor_eigen'):
+            sij[ix,iy,iz,:,:] += I
+            I_global += I
+        else:
+            #import pdb ; pdb.set_trace()
+            sij[ix,iy,iz,:,:] += v
+            A[ix,iy,iz,:] += V
+
         num[ix,iy,iz,:,:] += 1
 
-
+    import pdb ; pdb.set_trace()
     sij /= num
-    sij[np.isinf(sij)]=0
-    sij[np.isnan(sij)]=0
+    sij[np.isinf(sij)]=-9999.
+    sij[np.isnan(sij)]=-9999.
+
+    I_global/=np.sum(num)
+
+    for i in range(ngal):
+        print ("Processing galaxy error %d/%d"%(i,ngal))
+
+        # put this galaxy into a cell
+        info = shape_cat[i]
+        ix = np.argwhere((shape_cat['x'][i]>=lower) & (shape_cat['x'][i]<upper))[0,0]
+        iy = np.argwhere((shape_cat['y'][i]>=lower) & (shape_cat['y'][i]<upper))[0,0]
+        iz = np.argwhere((shape_cat['z'][i]>=lower) & (shape_cat['z'][i]<upper))[0,0]
+
+        # reconstruct the 3x3 shape matrix for this galaxy
+        a0 = np.array([shape_cat['av_x'][i],shape_cat['av_y'][i],shape_cat['av_z'][i]])
+        b0 = np.array([shape_cat['bv_x'][i],shape_cat['bv_y'][i],shape_cat['bv_z'][i]])
+        c0 = np.array([shape_cat['cv_x'][i],shape_cat['cv_y'][i],shape_cat['cv_z'][i]])
+        V = np.diag(np.array([shape_cat['a'][i]**2, shape_cat['b'][i]**2, shape_cat['c'][i]**2]))
+        v = np.array([a0,b0,c0])
+        v0=np.linalg.inv(v)
+        I = np.dot(v0,np.dot(V,v))
+
+        I_TF = get_trace_free_matrix(I) 
+
+        #print(I)
+        
+
+        # add it to the correct cell
+        dI_global += (I_TF - I_global) * (I_TF - I_global) 
+  
+
+    
+    dI_global /= np.sum(num)
+    dI_global = np.sqrt(dI_global)
+
+    for i in range(3):
+        for j in range(3):
+            dsij[:,:,:,i,j] = dI_global[i,j]
+
+    #import pdb ; pdb.set_trace()
+
+
+    if (average_type=='tensor_eigen'):
+        for ix in range(resolution):
+            for iy in range(resolution):
+                for iz in range(resolution):
+                    I0 = np.copy(sij[ix,iy,iz,:,:])
+                    if (I0==-9999.).all():
+                        continue
+                    _,v = np.linalg.eigh(I0)
+                    sij[ix,iy,iz,:,:] = v
+                    #import pdb ; pdb.set_trace()
 
     del(shape_cat)
     del(X)
@@ -86,17 +167,22 @@ def build_shape_cube(snap, resolution=512, npart=1024):
     del(upper)
     gc.collect()
 
-    return sij
+    import pdb ; pdb.set_trace()
+
+    return sij, dsij
 
 def gen_shape_cubes(snaps=[], resolution=512):
     """
     Outputs a fits file with the density cube for the corresponding snapshots
     """
     for sn in snaps:
-        gamma = build_shape_cube(sn, resolution=resolution)
-        outfits = fi.FITS("stellar_shape_%03d_%d_noavg.fits"%(sn,resolution), 'rw')
+        gamma, dgamma = build_shape_cube(sn, resolution=resolution)
+        outfits = fi.FITS("stellar_shape_%03d_%d.fits"%(sn,resolution), 'rw')
+        outfits2 = fi.FITS("stellar_shape_var_%03d_%d.fits"%(sn,resolution), 'rw')
         outfits.write(gamma)
         outfits.close()
+        outfits2.write(dgamma)
+        outfits2.close()
         #fits.writeto("stellar_shape_var_%03d.fits"%(sn), dgamma)
 
     print('Done all.')
@@ -126,7 +212,14 @@ def build_density_cube(snap, resolution=512, npart=600, ptype='dm'):
         print(j)
         header = dict(f['Header'].attrs.items())
         ptcl_coords = f[gName]["Coordinates"][:]/1000.
-        hist,edges = np.histogramdd(ptcl_coords, resolution, range=[[0,boxsize],[0,boxsize],[0,boxsize]])
+
+        if ptype=='dm':
+            Mp = None
+        else:
+            Mp = f[gName]['Masses'][:]
+
+        #import pdb ; pdb.set_trace()
+        hist,edges = np.histogramdd(ptcl_coords, resolution, range=[[0,boxsize],[0,boxsize],[0,boxsize]], weights=Mp)
         density = density + hist
         f.close()
         #i+=1
@@ -141,8 +234,8 @@ def gen_density_cubes(snaps=[], ptype='dm', resolution=512):
     """
     for sn in snaps:
         dens = build_density_cube(sn, ptype=ptype, resolution=resolution)
-        os.system("rm /Volumes/groke/%s_density_%03d_%d.fits"%(ptype,sn,resolution))
-        outfits = fi.FITS("/Volumes/groke/%s_density_%03d_%d.fits"%(ptype,sn,resolution),'rw')
+        os.system("rm %s_density_%03d_%d.fits"%(ptype,sn,resolution))
+        outfits = fi.FITS("%s_density_%03d_%d.fits"%(ptype,sn,resolution),'rw')
         outfits.write(dens)
         outfits.close()
 
@@ -174,6 +267,7 @@ def compute_tidal_tensor(dens, smoothing=0.25, pixel_size=0.1953):
     for i in range(3):
         for j in range(3):
             # k[i], k[j] are 3D matrices, as is k
+            import pdb ; pdb.set_trace()
             temp =  fft_dens * k[i]*k[j]/(k[0]**2 + k[1]**2 + k[2]**2)
             temp[0,0,0] = 0
 
@@ -193,7 +287,7 @@ def gen_tidal_tensors(snaps=[], smoothing=[], ptype='dm', resolution=512, box_si
     for i in snaps:
         dens = fi.FITS(dens_dir+'%s_density_%03d_%d.fits'%(ptype,i,resolution))[-1].read()
         for s in smoothing:
-            tid  = compute_tidal_tensor(dens, smoothing=s, pixel_size=box_size./resolution)
+            tid  = compute_tidal_tensor(dens, smoothing=s, pixel_size=box_size/resolution)
             out = fi.FITS(dens_dir+'%s_tidal_%03d_%0.2f_%d.fits'%(ptype,i,s,resolution),'rw') ; out.write(tid) ; out.close()
             # Diagonalise the tidal matrix while we are at it
             vals, vects = npl.eigh(tid)
